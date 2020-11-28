4a40c59ac1a3458f4553f8f1782a4bf111414c10
Ash515 <aaashwin515@gmail.com>
2020-11-28 12:18:09 +0530

    Added server.js file

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+const path=require('path')
 const express=require('express');
 const app=express();
+//Set Static folder
+app.use(express.static(path.join(__dirname,'Public')));
 const PORT=3000 ||process.env.PORT;
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
